fc39ae7ac5bfbe9aded05433ca706032306a0563
laksh <lakshyam38@gmail.com>
2022-06-28 16:28:09 +0530

    content type check

--- src/app/test_endpoints.py
+++ src/app/test_endpoints.py
@@ -7,7 +7,5 @@ client = TestClient(app)
 def test_read_index():
     response = client.get("/")
 
-    print(response.text)
-
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
