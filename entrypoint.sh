c2b2ac62aa39eb9fb508097a0d8a5fddbbd5116a
laksh <lakshyam38@gmail.com>
2022-07-01 13:48:29 +0530

    updated run command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 RUN_PORT=${PORT:-8000}
 
-/usr/local/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker app.main:app --bind "0.0.0.0:${RUN_PORT}"
+uvicorn app.main:app --bind "0.0.0.0:${RUN_PORT}"
