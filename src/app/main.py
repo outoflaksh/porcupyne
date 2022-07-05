4892907d942cd9661bda8a2ef9b834428fb796d2
laksh <lakshyam38@gmail.com>
2022-07-05 10:18:29 +0530

    Complete file upload echo test endpoint

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,9 @@
 import pathlib
+import io
+import uuid
 
-from fastapi import FastAPI, Request, Depends
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Depends, File, UploadFile
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 
 from functools import lru_cache
@@ -11,6 +13,9 @@ from .fastapi_settings import Settings
 
 # Define the base directory
 BASE_DIR = pathlib.Path(__file__).parent.parent
+
+# Define directory for uploads
+UPLOAD_DIR = BASE_DIR / "uploads"
 
 # Instantiate the FastAPI object
 app = FastAPI()
@@ -24,7 +29,29 @@ def get_settings():
     return Settings()
 
 
-# Home view -> HTTP GET
+# Home view -> http GET
 @app.get("/", response_class=HTMLResponse)
 def read_index(request: Request, settings: Settings = Depends(get_settings)):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+# Image upload test endpoint -> http POST
+@app.post("/img-echo", response_class=FileResponse)
+async def image_echo_view(file: UploadFile = File(...)):
+    # Read the bytes of the file
+    byte_str = io.BytesIO(await file.read())
+
+    # Extract metadata
+    fname = pathlib.Path(file.filename)  # name of uploaded file
+    fext = fname.suffix  # extension of uploaded file
+    abs_fname = str(fname).split(fext)[0]  # name of file without the ext
+
+    # Generate a unique destination for uploaded file
+    destination = UPLOAD_DIR / f"{abs_fname}-{uuid.uuid1()}{fext}"
+
+    # Save uploaded file at the generated destination
+    with open(str(destination), "wb") as out:
+        out.write(byte_str.read())
+
+    # Return file
+    return destination
